3049fef434ff51f15fafeaec7f1c7cb4e8e17174
hrishabh6 <hrishabhjoshi123@gmail.com>
2025-01-19 14:57:43 +0530

    Project frontend setup

---
+++ frontend/app/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import { auth, signIn, signOut } from '@/auth'
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = async () => {
+    const session = await auth()
+  return (
+    <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+      <nav className='flex justify-between items-center'>
+        <Link href='/'>
+            <p className='text-black'>Logo Goes here</p>
+        </Link>
+
+        <div className='flex items-center gap-5'>
+            {
+                session && session?.user ? (
+                    <>
+                        <Link  href='/search'>
+                            <p className='text-black'>Search for aluminis</p>
+                        </Link>
+                        <form action={async () => {
+                            "use server"
+                            await signOut(
+                                {
+                                    redirectTo : "/"
+                                }
+                            )
+                        }} className='text-black'>
+                            <button type='submit' className='text-black'>Log Out</button>
+                        </form>
+                        <Link href={`/user/${session?.id}`}>
+                            <span className='text-black '>{session?.user?.name}</span>
+                        </Link>
+                    </>
+                ) : 
+                ( 
+                    <form action={async () => {
+                        "use server"
+                        await signIn("google")
+                    }}>
+                        <button className='text-black' type='submit'>Login in</button>
+                    </form> 
+                )
+            }
+        </div>
+
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
