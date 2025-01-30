53a4c0284e4f00b8b45a45978e643e45b1c674b4
hrishabh6 <hrishabhjoshi123@gmail.com>
2025-01-30 22:10:53 +0530

    Made it more responsive

--- frontend/components/HeroSection.jsx
+++ frontend/components/HeroSection.jsx
@@ -3,24 +3,9 @@ import PostCreator from './shared/Post'
 
 const HeroSection = () => {
   return (
-    <div className='flex w-full max-md:mt-5 lg:px-10 flex-col justify-between gap-4  sm:items-center'>
+    <div className='flex w-full max-md:mt-5 md:pl-20 lg:px-10 flex-col justify-between gap-4  sm:items-center'>
       <PostCreator/>
-      <PostCreator/>
-      <PostCreator/>
-      <PostCreator/>
-      <PostCreator/>
-      <PostCreator/>
-      <PostCreator/>
-      <PostCreator/>
-      <PostCreator/>
-      <PostCreator/>
-      <PostCreator/>
-      <PostCreator/>
-      <PostCreator/>
-      <PostCreator/>
-      <PostCreator/>
-      <PostCreator/>
-      <PostCreator/>
+      
       
     </div>
   )
